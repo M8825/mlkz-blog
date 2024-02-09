c0ac5200166dcef4bfd6a07a96404e15839b6e50
Mlkz <maho.mamulashvili@gmail.com>
2024-02-09 13:27:11 -0500

    Add currently working on section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from './components/header/header.js'
+import CurrentlyDoing from './components/currentlyDoing/CurrentlyDoing.js'
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <CurrentlyDoing />
+    </>
 	);
 }
 
---
+++ src/components/currentlyDoing/CurrentlyDoing.js
@@ -0,0 +1,12 @@
+function CurrentlyDoing() {
+  return (
+    <div className="mx-auto my-12 max-w-2xl px-6">
+      <p className="my-3 text-medium inline-block font-medium no-underline">About</p>
+      <p className="text-gray-500">I work on this blog currently.</p>
+      <p className="text-gray-500">I like building things that affect everyday life of people around me.</p>
+      <p className="my-2 text-gray-500">Previously I worked at Go Consulting Group as backend ruby-on-rails engineer.</p>
+    </div>
+  )
+}
+
+export default CurrentlyDoing;
